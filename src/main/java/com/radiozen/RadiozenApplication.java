1eac28ea549c06a50b09c5ef7449eda2dc147728
Tomi2107 <83030653+Tomi2107@users.noreply.github.com>
2025-04-02 17:58:34 -0300

    Create RadiozenApplication.java
    Se cambio la hubicacion del archivo main

---
+++ src/main/java/com/radiozen/RadiozenApplication.java
@@ -0,0 +1,13 @@
+package com.radiozen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiozenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiozenApplication.class, args);
+	}
+
+}
