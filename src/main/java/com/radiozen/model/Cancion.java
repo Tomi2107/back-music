488e15c17e51adddeaca439a789d9d624fd47675
Tomi2107 <83030653+Tomi2107@users.noreply.github.com>
2025-04-04 20:56:34 -0300

    Update Cancion.java
    Se compagino con los campos actuales de firebase

--- src/main/java/com/radiozen/model/Cancion.java
+++ src/main/java/com/radiozen/model/Cancion.java
@@ -3,14 +3,22 @@ package com.radiozen.model;
 public class Cancion {
     private String titulo;
     private String artista;
+    private String album;
+    private int año;
+    private String duracion;
+    private String genero;
     private String url;
 
     // Constructor vacío requerido por Firestore
     public Cancion() {}
 
-    public Cancion(String titulo, String artista, String url) {
+    public Cancion(String titulo, String artista, String album, int año, String duracion, String genero, String url) {
         this.titulo = titulo;
         this.artista = artista;
+        this.album = album;
+        this.año = año;
+        this.duracion = duracion;
+        this.genero = genero;
         this.url = url;
     }
 
@@ -20,6 +28,18 @@ public class Cancion {
 
     public String getArtista() { return artista; }
     public void setArtista(String artista) { this.artista = artista; }
+
+    public String getAlbum() { return album; }
+    public void setAlbum(String album) { this.album = album; }
+
+    public int getAño() { return año; }
+    public void setAño(int año) { this.año = año; }
+
+    public String getDuracion() { return duracion; }
+    public void setDuracion(String duracion) { this.duracion = duracion; }
+
+    public String getGenero() { return genero; }
+    public void setGenero(String genero) { this.genero = genero; }
 
     public String getUrl() { return url; }
     public void setUrl(String url) { this.url = url; }
