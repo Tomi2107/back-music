0daa132652d9a9bd31fa166ee46d88aed24af002
Tomi2107 <83030653+Tomi2107@users.noreply.github.com>
2025-04-05 15:48:54 -0300

    Update SongController.java
    incopleto un import

--- src/main/java/com/radiozen/controller/SongController.java
+++ src/main/java/com/radiozen/controller/SongController.java
@@ -11,8 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation;
-
+import org.springframework.web.bind.annotation.RestController;  
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
