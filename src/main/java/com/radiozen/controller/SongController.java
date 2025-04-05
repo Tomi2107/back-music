b32b0c4a76ddc7afc57da7430063eb235f4cf22c
Tomi2107 <83030653+Tomi2107@users.noreply.github.com>
2025-04-05 15:03:45 -0300

    Update SongController.java
    Se agrego verificacion para archivo mp3

--- src/main/java/com/radiozen/controller/SongController.java
+++ src/main/java/com/radiozen/controller/SongController.java
@@ -11,7 +11,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+    System.out.println("Recibido archivo: " + archivo.getOriginalFilename());
+import org.springframework.web.bind.annotation{
+    return ResponseEntity.ok("Archivo recibido");
+}
+
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
